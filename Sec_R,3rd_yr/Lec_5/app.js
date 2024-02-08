a1594999e9ec8b908709ee661f960d552306dc39
sakshammishraGla <sakshamgla72@gmail.com>
2024-02-08 15:09:54 +0530

    sec r crud

--- Sec_R,3rd_yr/Lec_5/app.js
+++ Sec_R,3rd_yr/Lec_5/app.js
@@ -3,11 +3,13 @@ const app = express()
 const path = require("path")
 const PORT = 4444;
 const hbs = require("hbs")
+const { v4: uuidv4 } = require('uuid');
 
 let blogs = []
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
@@ -32,8 +34,10 @@ app.post("/addblog", (req, res) => {
     const obj={
        name,
        Class,
-       blog
+       blog,
+       blogId:uuidv4()
     }
+    // console.log(obj);
     blogs.push(obj)
     res.redirect("/getblog")
 })
@@ -42,6 +46,39 @@ app.get("/getblog",(req,res)=>{
     res.render("blogpage",{
         blogs
     })
+})
+
+app.get("/delete/:blogId",(req,res)=>{
+    // console.log(req.params);
+    blogs=blogs.filter((item)=>item.blogId!=req.params.blogId)
+    res.redirect("/getblog")
+})
+
+app.get("/update/:blogId",(req,res)=>{
+    const updateblog=blogs.filter((item)=>item.blogId==req.params.blogId)
+    // console.log(updateblog);
+    res.render("updateblog",{
+        updateblog:updateblog[0]
+    })
+})
+
+app.post("/updateblog",(req,res)=>{
+    const {name,Class,blog,blogId}=req.body
+    const newObj={
+        name,
+        Class,
+        blog,
+        blogId
+    }
+
+    blogs=blogs.map((item)=>{
+        if(item.blogId==blogId){
+            return newObj
+        }
+        return item
+    })
+
+    res.redirect("/getblog")
 })
 
 app.listen(PORT, () => {
