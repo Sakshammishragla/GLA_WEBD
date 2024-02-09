aa613d24f534b1e1330c73bc992c85a38f231893
sakshammishraGla <sakshamgla72@gmail.com>
2024-02-09 10:14:14 +0530

    crud sec s

--- Sec_S,3rd_yr/Lec_5/app.js
+++ Sec_S,3rd_yr/Lec_5/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const PORT = 4444;
-const hbs=require("hbs")
+const hbs = require("hbs")
 const { v4: uuidv4 } = require('uuid');
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -12,45 +12,64 @@ app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials');
 
-let blogs=[]
+let blogs = []
 
-app.post("/addblog",(req,res)=>{
-    // console.log(req.body);
-    const {name,Class,blog}=req.body
-    const obj={
-        name:name,
-        Class:Class,
-        blog:blog,
-        blogId:uuidv4()
-    }
-   console.log(obj);
-    blogs.push(obj)
-    // res.send(blogs)
-    res.redirect('/getblog')
+app.post("/addblog", (req, res) => {
+  // console.log(req.body);
+  const { name, Class, blog } = req.body
+  const obj = {
+    name: name,
+    Class: Class,
+    blog: blog,
+    blogId: uuidv4()
+  }
+  console.log(obj);
+  blogs.push(obj)
+  // res.send(blogs)
+  res.redirect('/getblog')
 })
 
 
 
-app.get("/getblog",(req,res)=>{
-    res.render("blogpage",{
-        blogs:blogs
-    })
-})
-
-app.get("/delete/:blogId",(req,res)=>{
-    // console.log(req.params);
-    blogs=blogs.filter((item)=>item.blogId!=req.params.blogId)
-    res.redirect("/getblog")
-})
-
-app.get("/update/:blogId",(req,res)=>{
-  const updateblog=blogs.filter((item)=>item.blogId==req.params.blogId)
-//   console.log(updateblog);
-  res.render("updateblog",{
-    updateblog:updateblog[0]
+app.get("/getblog", (req, res) => {
+  res.render("blogpage", {
+    blogs: blogs
   })
 })
 
+app.get("/delete/:blogId", (req, res) => {
+  // console.log(req.params);
+  blogs = blogs.filter((item) => item.blogId != req.params.blogId)
+  res.redirect("/getblog")
+})
+
+app.get("/update/:blogId", (req, res) => {
+  const updateblog = blogs.filter((item) => item.blogId == req.params.blogId)
+  console.log(updateblog);
+  res.render("updateblog", {
+    updateblog: updateblog[0]
+  })
+})
+
+app.post("/updateblog", (req, res) => {
+  const { name, Class, blog, blogId } = req.body
+  const newObj = {
+    name,
+    Class,
+    blog,
+    blogId
+  }
+
+  blogs = blogs.map((item) => {
+    if (item.blogId == blogId) {
+      return newObj
+    }
+    return item
+  })
+
+  res.redirect("/getblog")
+})
+
 app.listen(PORT, () => {
-    console.log("http://localhost:" + PORT);
+  console.log("http://localhost:" + PORT);
 })
