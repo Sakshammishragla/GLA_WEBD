7601f5ff98a5d273ff7f61266902cfe29dffad02
sakshammishraGla <sakshamgla72@gmail.com>
2024-03-11 14:31:03 +0530

    model router in sec A X

---
+++ Sec_X,(AH),3rd_yr/Lec_8/model/Blog.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require("mongoose");
+const {Schema}=mongoose
+
+const blogSchema=new Schema({
+    name:String,
+    Class:String,
+    blog:String,
+    blogId:String
+})
+
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports=Blog
---
+++ Sec_X,(AH),3rd_yr/Lec_8/router/blog.js
@@ -0,0 +1,56 @@
+const express=require("express")
+const Blog=require("../model/Blog")
+const { v4: uuidv4 } = require('uuid');
+const router=express.Router()
+
+router.get("/getblogs",async(req,res)=>{
+    let blogs=await Blog.find()
+     res.render('blogpage',{
+      blogs   
+     })
+ })
+
+ router.post("/addblog",async(req,res)=>{
+    const {name,Class,blog}=req.body
+    const obj={
+        name,
+        Class,
+        blog,
+        blogId:uuidv4()
+    }
+    await Blog.create(obj)
+    res.redirect("/getblogs")
+})
+
+router.get("/delete/:blogId",async(req,res)=>{
+    let blogId=req.params.blogId
+    await Blog.deleteOne({blogId})
+    res.redirect("/getblogs")
+})
+
+router.get("/update/:blogId",async(req,res)=>{
+    let blogId=req.params.blogId
+    const updateblog=await Blog.findOne({blogId})
+    
+    res.render("updateblog",{
+        updateblog:updateblog
+    })
+})
+
+
+
+router.post("/updateblog",async(req,res)=>{
+    const {name,Class,blog,blogId}=req.body
+    const newObj={
+        name,
+        Class,
+        blog,
+        blogId
+    }
+   
+    await Blog.updateOne({blogId},newObj)
+
+    res.redirect("/getblogs")
+})
+
+module.exports=router
