17b9b05fdeece5917e6b8b3334e2eac10f050acf
sakshammishraGla <sakshamgla72@gmail.com>
2024-03-18 14:55:35 +0530

    ecommerce in sec AX

---
+++ Sec_X,(AH),3rd_yr/Lec_10(eCommerce)/model/Product.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+const {Schema}=mongoose
+
+const productSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true,
+        min:0
+    },
+    img:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    }
+})
+
+const Product=mongoose.model("Product",productSchema)
+module.exports=Product
