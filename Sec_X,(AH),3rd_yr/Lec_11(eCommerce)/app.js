19010e3da0cc0279b866e1971cbc58cd49949ea2
sakshammishraGla <sakshamgla72@gmail.com>
2024-03-19 11:33:14 +0530

    eCommerce in sec AX

---
+++ Sec_X,(AH),3rd_yr/Lec_11(eCommerce)/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 4444
+const hbs=require("hbs")
+const mongoose=require("mongoose")
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials', function (err) {})
+app.use(express.static(path.join(__dirname,"public")))
+app.use("/",require("./routes/product"))
+
+mongoose.connect('mongodb://127.0.0.1:27017/eCommerce_secAX')
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('http://localhost:' + PORT)
+        })
+    })
