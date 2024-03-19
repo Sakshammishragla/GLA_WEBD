b03918b9613334a48bf49cd34cef5fa9aa6ed2aa
sakshammishraGla <sakshamgla72@gmail.com>
2024-03-19 10:54:24 +0530

    reviews

--- Sec_T,3rd_yr/Lec_11(eCommerce)/routes/review.js
+++ Sec_T,3rd_yr/Lec_11(eCommerce)/routes/review.js
@@ -7,16 +7,14 @@ router.get("/product/:id/addreviews", async(req, res) => {
     console.log("reviews");
     const { rating, comment } = req.query
     let id = req.params.id
-    console.log(id);
-    let product = Product.findOne({_id:id})
-    console.log("product",product.reviews);
+    let product =await Product.findById(id)
     let review = new Review({ rating, comment })
     product.reviews.push(review)
 
    await product.save()
    await review.save()
 
-   res.redirect(`/product/${id}`)
+   res.redirect(`/products/${id}`)
 })
 
 module.exports = router
