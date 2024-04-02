bd2f7cf274fc68e01bad0204b3f8a9ed63092667
sakshammishraGla <sakshamgla72@gmail.com>
2024-04-02 10:38:46 +0530

    client side validation in sec S

---
+++ Sec_S,3rd_yr/Lec_11(eCommerce)/routes/product.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const Product = require("../model/Product")
+const router = express.Router()
+
+router.get("/products", async (req, res) => {
+   let products = await Product.find()
+   res.render("allProducts", { products })
+})
+
+router.get("/product/:id", async (req, res) => {
+   let id = req.params.id
+   let product = await Product.findById(id).populate("reviews")
+   console.log(product);
+   res.render("productPage", { product })
+})
+
+router.get("/addproduct", (req, res) => {
+   res.render("addProduct")
+})
+
+router.post("/addproduct", async (req, res) => {
+   const { name, price, img, desc } = req.body
+   await Product.create({ name, price, img, desc })
+   res.redirect("/products")
+})
+
+module.exports = router
