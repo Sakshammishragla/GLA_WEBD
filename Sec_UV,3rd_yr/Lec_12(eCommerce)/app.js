5888b222b5a2da8dd4ef2a597d0747f39a0ba153
sakshammishraGla <sakshamgla72@gmail.com>
2024-04-02 08:41:12 +0530

    client side validation in sec uv

---
+++ Sec_UV,3rd_yr/Lec_12(eCommerce)/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 4444
+const mongoose=require("mongoose")
+const hbs =require("hbs")
+
+app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+app.use(express.static(path.join(__dirname,'public')))
+app.use("/",require("./routes/product"))
+app.use("/",require("./routes/review"))
+
+mongoose.connect('mongodb://127.0.0.1:27017/eCommerce_secUV')
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('http://localhost:' + PORT)
+        })
+    })
