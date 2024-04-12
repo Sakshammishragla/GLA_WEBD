9883ec769422922486764d144f3cb27fe9e198ef
sakshammishraGla <sakshamgla72@gmail.com>
2024-04-12 11:49:42 +0530

    session cookie

---
+++ Sec_UV,3rd_yr/Lec_15(eCommerce)/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 4444
+const mongoose=require("mongoose")
+const hbs =require("hbs")
+const session = require('express-session')
+const flash = require('connect-flash');
+
+app.use(flash());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+app.use(express.static(path.join(__dirname,'public')))
+app.use("/",require("./routes/product"))
+app.use("/",require("./routes/review"))
+
+mongoose.connect('mongodb://127.0.0.1:27017/eCommerce_secUV')
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('http://localhost:' + PORT)
+        })
+    })
